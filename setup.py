e10ce5a03c1cd3b983b0a7b5587b633a0bf672d5
AndreMarcato <andre.marcato@ufjf.edu.br>
2024-01-25 19:27:48 -0300

    Versao Teste Pré Arquivos Térmicas 3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools.command.install import install
 from setuptools import find_packages
 
 # current version
-VERSION = "0.0.50"
+VERSION = "0.0.51"
 
 def readme():
     """print long description"""
