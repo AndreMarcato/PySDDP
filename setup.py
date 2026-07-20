fb208aebdc886cd82f28bc6f87250c0a72aedb3a
AndreMarcato <andre.marcato@ufjf.edu.br>
2026-07-20 17:54:12 -0300

    Release 0.0.68

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools.command.install import install
 from setuptools import find_packages
 
 # current version
-VERSION = "0.0.67"
+VERSION = "0.0.68"
 
 def readme():
     """print long description"""
@@ -66,4 +66,5 @@ setup(
         'verify': VerifyVersionCommand,
     }
 )
+
 
